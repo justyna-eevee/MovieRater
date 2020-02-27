d391369393c4217e124733d6262f7203b8895ab3
Mimimeczka <justyna.zaczyk@onet.pl>
2020-02-27 20:39:02 +0100

    whole project

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,4 @@ from .models import Movie
 class MovieForm(ModelForm):
     class Meta:
         model = Movie
-        fields = ["name", "description", "year", "released", "imdb_rating", "photo"]
+        fields = ["name", "description", "year", "released", "imdb_rating", "photo", "info"]
--- movierater/settings.py
+++ movierater/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,11 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iorr^_&68)1gbj7sac3jjs_v8i1je)wqj#&^f#shx-#@vd7qn)'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -72,16 +72,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'movierater.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = {'default': config('DATABASE_URL',default=default_dburl, cast=dburl)}
 
 
 # Password validation
@@ -122,7 +114,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ['moje_static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'moje_static')
+
+# STATICFILES_DIRS = ['moje_static']
 
 MEDIA_URL = '/media/'
 
